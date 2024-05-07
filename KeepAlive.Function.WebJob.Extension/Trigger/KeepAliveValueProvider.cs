543159157384247433ad0c2ce5f3f7b56f1a20bd
Tim Wigmore-Welsh <tim@wigmore-welsh.com>
2024-05-07 16:56:04 +0100

    Handle converting KeepAliveValue to string

--- KeepAlive.Function.WebJob.Extension/Trigger/KeepAliveValueProvider.cs
+++ KeepAlive.Function.WebJob.Extension/Trigger/KeepAliveValueProvider.cs
@@ -1,21 +1,31 @@
 using System.Reflection;
 using Microsoft.Azure.WebJobs.Host.Bindings;
+using Newtonsoft.Json;
 
 namespace Webjob.Extensions.KeepAlive.Trigger;
 
 internal class KeepAliveValueProvider : IValueProvider
 {
+    private static readonly Type STRING_TYPE = typeof(string);
+    
     private readonly object _value;
     private readonly ParameterInfo _parameter;
+    private readonly bool _parameterTypeIsString;
 
     public KeepAliveValueProvider(object value, ParameterInfo parameter)
     {
         _value = value;
         _parameter = parameter;
+        _parameterTypeIsString = parameter.ParameterType == STRING_TYPE;
     }
 
     public async Task<object> GetValueAsync()
     {
+        if (_parameterTypeIsString)
+        {
+            return Task.FromResult((object)JsonConvert.SerializeObject(_value));
+        }
+        
         return _value;
     }
 
